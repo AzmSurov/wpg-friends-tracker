757c262c12f8e82f42d211a4f6c7e83d9aedb77a
AZM Mohibul Surov <azmsm1@AZMs-MacBook-Pro.local>
2023-06-10 18:06:55 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,147 @@
-import logo from './logo.svg';
+/* eslint-disable react/jsx-no-undef */
+
 import './App.css';
+import Header from './components/Header';
+import toast, { Toaster } from 'react-hot-toast';
+import avatar1 from "./images/avatar-1.png"
+import avatar2 from "./images/avatar-2.png"
+import avatar3 from "./images/avatar-3.png"
+import avatar4 from "./images/avatar-4.png"
+import avatar5 from "./images/avatar-5.png"
+import avatar6 from "./images/avatar-6.png"
+import avatar7 from "./images/avatar-1.png"
+import {AiOutlineFilter} from 'react-icons/ai'
+const Highest = () => toast('Winner Winner Chicken Dinner!', {
+  icon: '👏',
+});
+
+const second = () => toast("Second place is just the first place loser...but hey, at least you're the best at not being the best!", {
+  icon: '👏',
+});
+const poop = () => toast("I poop 💩💩💩💩 💩💩💩💩", {
+  icon: '💩',
+});
+const last = () => toast("Last place is just the universe's way of reminding us that the journey matters more than the destination", {
+  icon: '👏',
+});
+
+
 
 function App() {
+
+  const wins = [
+    {
+        id:1,
+      avatar: avatar2,
+      name: "Tizzi",
+      total: 3,
+      status: "Highest",
+      toasty: Highest
+    },
+    {
+        id:2,
+      avatar: avatar1,
+      name: "KingKar",
+      total: 2,
+      status: "Second",
+      toasty: second
+    },
+    {
+        id:3,
+      avatar: avatar6,
+      name: "Kabbo",
+      total: 0,
+      status: "Last",
+      toasty : last
+    },
+    {
+        id:4,
+      avatar: avatar4,
+      name: "Nishi",
+      total: 2,
+      status: "Second",
+      toasty: second
+    },
+    {
+        id:5,
+      avatar: avatar6,
+      name: "Imtiaz",
+      total: 1,
+      status: "💩💩💩💩",
+      toasty: poop
+    },
+    {
+      id:6,
+    avatar: avatar1,
+    name: "Swrov",
+    total: 0,
+    status: "Last",
+    toasty : last
+  },
+  {
+    id:7,
+    avatar: avatar5,
+    name: "Neon",
+    total: 2,
+    status: "Second",
+    toasty: second
+    
+  }
+    
+  ];
+
+  wins.sort((a, b) => b.total - a.total);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App bg-black min-h-screen">
+     <Header />
+     <div>
+        <div class="p-6 bg-gray-900 rounded-lg">
+    <div class="flex justify-between items-center pb-4">
+      <h2 class="text-xl font-semibold leading-loose text-white">Game Stats</h2>
+      <button class="flex py-3 px-4 rounded-lg border border-gray-700 gap-x-2.5 text-white">
+        <AiOutlineFilter className='' size={20}/>
+        <span class="text-sm">UNO</span>
+      </button>
     </div>
+    <table class="w-full">
+      <thead>
+        <tr class="text-sm font-semibold text-white">
+          <td class="py-4 border-b border-gray-700">Name</td>
+          <td class="py-4 border-b border-gray-700 text-right">Total Wins</td>
+          <td class="py-4 border-b border-gray-700 text-center">Status</td>
+        </tr>
+      </thead>
+      <tbody className=''>
+
+          {wins.map(({id, avatar, name, total, status, toasty}) => (
+        <tr key={id}  class="text-sm text-gray-400">
+
+              <td class="py-4">
+                <div class="flex gap-4 items-center">
+                  <img onClick={toasty} className='rounded-full cursor-pointer h-10 w-10' width="32" src={avatar} alt="" />
+                  <span > {name} </span>
+                </div>
+              </td>
+              <td class="py-4 tabular-nums text-center">{total}</td>
+              <td class="py-4 flex justify-center pl-2">
+                <span
+                  class={"flex justify-center py-1 w-24 font-medium capitalize rounded-full " + (status === "Highest" ? "bg-[#50D1AA]/20 text-[#50D1AA]" : "") + (status === "Second" ? "bg-[#9290FE]/20 text-[#9290FE]" : "") + (status === "Last" ? "bg-[#ff7b72]/20 text-[#ff7b72]" : "") + (status === "💩💩💩💩" ? "bg-[#964B00]/20 text-[#964B00]" : "")}
+                >
+                  {status}
+                </span>
+              </td>
+
+              </tr> 
+          ))}
+
+      </tbody>
+    </table>
+  </div>
+    </div> <Toaster />
+    
+    </div>
+    
   );
 }
 
---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className=''>
+        <nav className='flex  items-center justify-center py-4 sm:py-6 w-full text-white bg-black border-b border-gray-700 '>
+        <div class="">
+    <a href="/" class="text-lg sm:text-2xl  font-bold uppercase hover:text-blue-dark">Winnipeg Friends Tracker</a>
+  </div>
+        </nav>
+    </div>
+  )
+}
+
+export default Header
